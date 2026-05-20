e623feb7a175b41e218901a2b640074a89ced99d
aminalg <mohammedaminealg.66@gmail.com>
2026-05-20 22:28:47 +0100

    66526

--- apps/api/src/modules/disputes/disputes.module.ts
+++ apps/api/src/modules/disputes/disputes.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { DisputesController } from './disputes.controller';
 import { DisputesService } from './disputes.service';
 import { NotificationsModule } from '../notifications/notifications.module';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [AuthModule, NotificationsModule],
+  imports: [NotificationsModule],
   controllers: [DisputesController],
   providers: [DisputesService],
   exports: [DisputesService],
